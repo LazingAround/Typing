cc635162ad88908bb938afa56cfbfedbc46eb6c8
Andrew Dong <andrew.dong@honeyinsurance.com>
2026-01-09 16:42:22 +1100

    Stuff

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
+const readerElement = document.getElementById('reader')
+const statusElement = document.getElementById('shower')
+let typed = []
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+  console.log(key === 'Backspace')
+  if (key === "Backspace") {
+    typed.splice(-1, 1)
+  }
+  else {
+    if (key != "Shift" && key != "Tab") {
+      typed.push(event.key)
+    }
+  }
+  updateInput()
+})
 
-
-setupCounter(document.querySelector('#counter'))
+function updateInput() {
+  statusElement.textContent = typed.join("")
+}
