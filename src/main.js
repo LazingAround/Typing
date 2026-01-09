2f4272a0f4dcd842f866209f62271b8b475bf14d
Andrew Dong <andrew.dong@honeyinsurance.com>
2026-01-09 17:23:20 +1100

    Word detection done

--- src/main.js
+++ src/main.js
@@ -1,22 +1,43 @@
 import './style.css'
 
-const readerElement = document.getElementById('reader')
-const statusElement = document.getElementById('shower')
+const theWord = document.getElementById('word')
+
+const word = "Hello"
+const letters = word.split('');
+letters.forEach((letter) => {
+  const l = document.createElement('letter')
+  l.textContent = letter
+  theWord.appendChild(l)
+})
+
+let index = 0
 let typed = []
+
 document.addEventListener('keydown', (event) => {
   const key = event.key
-  console.log(key === 'Backspace')
   if (key === "Backspace") {
     typed.splice(-1, 1)
+
+    index = Math.max(0, index - 1)
+    theWord.children[index].className = ""
   }
   else {
-    if (key != "Shift" && key != "Tab") {
-      typed.push(event.key)
+    if (key != "Shift" && key != "Tab" && key != "Enter" && key != "Escape" && key != "CapsLock" && key != "Control" && key != "Alt" && key != "Meta" && key != "ArrowUp" && key != "ArrowRight" && key != "ArrowDown" && key != "ArrowLeft") {
+      if (typed.length < letters.length) {
+        typed.push(key)
+      }
+      index = Math.min(index + 1, letters.length)
     }
   }
   updateInput()
 })
 
+const statusElement = document.getElementById('shower')
 function updateInput() {
-  statusElement.textContent = typed.join("")
+
+  typed.forEach((element, index) => {
+
+    theWord.children[index].className = (element === letters[index]) ? "correct" : "incorrect"
+  })
 }
+
